1fb5c80284496f13ee1c1f1f8be494c3e931d1b1
Michael Alejandro Ura A <ingmicha@gmail.com>
2019-06-21 15:30:01 -0500

    sh
    sh

--- app/src/main/java/com/mura/demo/MainActivity.kt
+++ app/src/main/java/com/mura/demo/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Toast
 import com.mura.demo.BuildConfig.VERSION_CODE
 import com.mura.demo.BuildConfig.VERSION_NAME
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,6 +13,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val message = "$VERSION_CODE $VERSION_NAME"
+        text.text = message
         Toast.makeText(this,message,Toast.LENGTH_LONG).show()
     }
 }
