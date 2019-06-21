91dad115fb1e0a41bb6b2a206ed63c556124e9c1
Michael Alejandro Ura A <ingmicha@gmail.com>
2019-06-21 11:33:57 -0500

    sh
    sh

--- scripts/script.sh
+++ scripts/script.sh
@@ -32,35 +32,22 @@ chmod +x ${propertiesFile}
 
 # update key properties based on build type
 if [ $buildType = 'debug' ]; then
-	(setProperty "KEYSTORE" "debug")
-	(setProperty "STORE_PASSWORD" "123456")
-	(setProperty "KEY_ALIAS" "debug")
-	(setProperty "KEY_PASSWORD" "123456")
+    fastlane debug
+#	(setProperty "KEYSTORE" "debug")
+#	(setProperty "STORE_PASSWORD" "123456")
+#	(setProperty "KEY_ALIAS" "debug")
+#	(setProperty "KEY_PASSWORD" "123456")
 elif [ $buildType = 'release' ]; then
-	(setProperty "KEYSTORE" "release")
-	(setProperty "STORE_PASSWORD" "$storePass")
-	(setProperty "KEY_ALIAS" "$keyAlias")
-	(setProperty "KEY_PASSWORD" "$keyPass")
+#	(setProperty "KEYSTORE" "release")
+#	(setProperty "STORE_PASSWORD" "$storePass")
+#	(setProperty "KEY_ALIAS" "$keyAlias")
+#	(setProperty "KEY_PASSWORD" "$keyPass")
+	fastlane release KEYSTORE:keystore/release STORE_PASSWORD:${storePass} KEY_ALIAS:${keyAlias} KEY_PASSWORD:${keyPass}
 fi
-
-# clean project
-chmod +x gradlew
-./gradlew clean --stacktrace
-
-
-cat << "EOF"
-   __                 _     _
-  / _|               | |   | |
- | |_    __ _   ___  | |_  | |   __ _   _ __     ___
- |  _|  / _` | / __| | __| | |  / _` | | '_ \   / _ \
- | |   | (_| | \__ \ | |_  | | | (_| | | | | | |  __/
- |_|    \__,_| |___/  \__| |_|  \__,_| |_| |_|  \___|
-
-
-EOF
-
-fastlane test
-
+#
+## clean project
+#chmod +x gradlew
+#./gradlew clean --stacktrace
 ## build
 #if [ $buildType = 'debug' ]; then
 #	./gradlew assembleDebug --stacktrace
