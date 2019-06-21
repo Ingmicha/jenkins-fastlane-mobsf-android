ad32c4d8269d63df54f3fae87da867ba36d15a97
Michael Alejandro Ura A <ingmicha@gmail.com>
2019-06-21 11:46:12 -0500

    sh
    sh

--- app/src/main/java/com/mura/demo/MainActivity.kt
+++ app/src/main/java/com/mura/demo/MainActivity.kt
@@ -2,11 +2,16 @@ package com.mura.demo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.mura.demo.BuildConfig.VERSION_CODE
+import com.mura.demo.BuildConfig.VERSION_NAME
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val message = "$VERSION_CODE $VERSION_NAME"
+        Toast.makeText(this,message,Toast.LENGTH_LONG).show()
     }
 }
